603bf748e07a2c7013d2d5d6e8a89ca8a0e971cb
Tarek <duceria@gmail.com>
2025-09-26 17:39:31 +0200

    new change

--- src/main/java/security/SecurityConfig.java
+++ src/main/java/security/SecurityConfig.java
@@ -29,7 +29,6 @@ public class SecurityConfig {
                 )
                 .httpBasic(Customizer.withDefaults())
                .build();
-
     }
 
 }
--- src/main/java/service/AddressService.java
+++ src/main/java/service/AddressService.java
@@ -1,16 +1,34 @@
 package service;
 
+import dao.AddressDao;
 import model.Address;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class AddressService {
+    private final AddressDao dao;
+
+    public AddressService(AddressDao dao) {
+        this.dao = dao;
+    }
+
+    @Transactional(readOnly = true)
     public Address findById(Long id) {
+       return dao.findById(id);
+
     }
 
-    public List<Address> findAll() {
-    }
+//    // jag antar att bara chef kan få göra detta men det krävs inte på uppgiften
+//    @Transactional(readOnly = true)
+//    public List<Address> findAll() {
+//        return dao.findAll();
+//    }
 
+    @Transactional
     public Address create(Address address) {
+        return dao.create(address);
     }
 }
